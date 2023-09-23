9f6552877e30964de9c0b5df37e90d4ebbfeaecc
MoonLight022 <144927769+MoonLight022@users.noreply.github.com>
2023-09-23 20:00:25 +0530

    Update send_gmail.py

--- send_gmail.py
+++ send_gmail.py
@@ -1,10 +1,10 @@
 import smtplib as s
 
-server = s.SMTP('smtp.gmail.com', 587) # 1st arguement is server name and 2nd is port number
+server = s.SMTP('smtp.gmail.com', 587) # 1st arguement: server name, and 2nd argument: server port number
 server.ehlo() # ehlo method identify the domain name of the sending host to SMTP
 server.starttls() # starttls method tells the email server to swith connection from insecure to secure
 sender_mail = "Sender Gmail Address"
-app_password = "App Password"
+app_password = "App Password" # You can get "App Password" from your google accoint. Go: Your Account > Security > App Password > Create. NOTE: you can get App Password only if your 2-step verification is on.
 sub = input("Subject: ")
 body = input("body: ")
 receiver_add = input('Receiver Gmail Address: ')
